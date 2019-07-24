4414ac5a804c965e15c04769041434f416f347d0
erez.efrat <erez.efrat@overwolf.com>
2019-07-24 17:20:19 +0300

    added copy buttons

--- native/windows/in-game/in-game-view.js
+++ native/windows/in-game/in-game-view.js
@@ -7,11 +7,20 @@ define([
       super();
 
       this.eventsLog = document.getElementById('eventsLog');
-      this.updatesLog = document.getElementById('updatesLog');
+      this.infoLog = document.getElementById('infoLog');
+      this.copyEventsButton = document.getElementById('copyEvents');
+      this.copyInfoButton = document.getElementById('copyInfo');
 
       this.logEvent = this.logEvent.bind(this);
       this.logInfoUpdate = this.logInfoUpdate.bind(this);
+      this.copyEventsLog = this._copyEventsLog.bind(this);
+      this.copyInfoLog = this._copyInfoLog.bind(this);
+
+      this.copyEventsButton.addEventListener('click', this._copyEventsLog);
+      this.copyInfoButton.addEventListener('click', this._copyInfoLog);
     }
+
+    // -- Public --
 
     // Add a line to the events log
     logEvent(string, isHighlight) {
@@ -19,8 +28,41 @@ define([
     }
     // Add a line to the info updates log
     logInfoUpdate(string, isHighlight) {
-      this._logLine(this.updatesLog, string, isHighlight);
+      this._logLine(this.infoLog, string, isHighlight);
     }
+
+    // -- Private --
+
+    _copyEventsLog() {
+      this._copyLog(this.eventsLog);
+    }
+
+    _copyInfoLog() {
+      this._copyLog(this.infoLog);
+    }
+
+    // Copy text from log
+    _copyLog(log) {
+      // Get text from all span children
+      const nodes = log.childNodes;
+      let text = '';
+      for (let node of nodes) {
+        if (node.tagName === 'SPAN') {
+          text += node.textContent + '\n';
+        }
+      }
+
+      // Create temporary textarea to copy to clipboard from
+      const textarea = document.createElement("textarea");
+      textarea.value = text;
+      textarea.setAttribute('readonly', '');
+      textarea.style = {position: 'absolute', left: '-9999px'};
+      document.body.appendChild(textarea);
+      textarea.select();
+      document.execCommand('copy');
+      document.body.removeChild(textarea);
+    }
+
 
     // Add a line to a log
     _logLine(log, string, isHighlight) {
