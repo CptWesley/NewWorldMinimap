54aea2845e674c9d4a95bc503735ef5919c216cd
Wesley Baartman <w.j.baartman@student.tudelft.nl>
2021-10-10 00:40:26 +0200

    Add basic map drawing

--- overwolf/src/logic/tiles.ts
+++ overwolf/src/logic/tiles.ts
@@ -4,6 +4,7 @@ const tileWidth = 256;
 const tileHeight = 256;
 const gameMapWidth = 14336;
 const gameMapHeight = 14400;
+const radius = 2;
 
 export function getTileCoordinatesForWorldCoordinate(x: number, y: number) {
     const totalWidth = width * tileWidth;
@@ -37,6 +38,20 @@ function getTileCacheKey(x: number, y: number) {
     return `${x},${y}`;
 }
 
+export function toMinimapCoordinate(playerX: number, playerY: number, x: number, y: number) {
+    const totalWidth = tileWidth * width;
+    const totalHeight = tileHeight * height;
+    const { x: tileX, y: tileY } = getTileCoordinatesForWorldCoordinate(playerX, playerY);
+
+    const pixelX = Math.floor(x / gameMapWidth * totalWidth);
+    const pixelY = Math.floor((gameMapHeight - y) / gameMapHeight * totalHeight);
+
+    const imageX = pixelX - ((tileX - radius) * tileWidth);
+    const imageY = pixelY - ((tileY - radius + 1) * tileHeight);
+
+    return { x: imageX, y: imageY };
+}
+
 const tileBitmapCache = new Map<string, Promise<ImageBitmap>>();
 export async function getTileBitmap(x: number, y: number) {
     const key = getTileCacheKey(x, y);
@@ -48,4 +63,24 @@ export async function getTileBitmap(x: number, y: number) {
     }
 
     return await bitmapPromise;
+}
+
+export function getTiles(x: number, y: number) {
+    const dimension = radius * 2 + 1;
+    const result: Promise<ImageBitmap>[][] = [];
+
+    const { x: centerX, y: centerY } = getTileCoordinatesForWorldCoordinate(x, y);
+
+    console.log('x: ' + centerX);
+    console.log('y: ' + centerY);
+
+    for (let x = 0; x < dimension; ++x) {
+        const col: Promise<ImageBitmap>[] = [];
+        result.push(col);
+        for (let y = 0; y < dimension; ++y) {
+            col.push(getTileBitmap(centerX - radius + x, centerY - radius + y));
+        }
+    }
+
+    return result;
 }
