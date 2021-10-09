db8768b39b67b9d4b0ef8b062f9ec2b86a4e985a
pvdstel <pvdstel@users.noreply.github.com>
2021-10-09 23:38:07 +0200

    Move .NET app source code to its own directory

---
+++ dotnet/src/NewWorldMinimap/Program.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Runtime.InteropServices;
+using System.Windows.Forms;
+
+namespace NewWorldMinimap
+{
+    /// <summary>
+    /// Entry point of the program.
+    /// </summary>
+    public static class Program
+    {
+        /// <summary>
+        /// Defines the entry point of the application.
+        /// </summary>
+        [STAThread]
+        public static void Main()
+        {
+            NativeMethods.SetProcessDPIAware();
+            Application.EnableVisualStyles();
+            using Form map = new MapForm();
+            Application.Run(map);
+        }
+
+        private static class NativeMethods
+        {
+            [DllImport("user32")]
+            [DefaultDllImportSearchPaths(DllImportSearchPath.System32)]
+            public static extern bool SetProcessDPIAware();
+        }
+    }
+}
