b33a834b0d1941c444c495958dac6a44c1e62df9
pvdstel <pvdstel@users.noreply.github.com>
2021-10-09 23:27:02 +0200

    Add tile logic and basic drawing

--- ts/src/OverwolfWindows/desktop/desktop.ts
+++ ts/src/OverwolfWindows/desktop/desktop.ts
@@ -1,14 +1,6 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import App from '../../App';
 import Desktop from '../../Desktop';
-import { AppWindow } from '../AppWindow';
-import { windowNames } from '../consts';
-
-// The desktop window is the window displayed while Portal 2 is not running.
-// In our case, our desktop window has no logic - it only displays static data.
-// Therefore, only the generic AppWindow class is called.
-new AppWindow(windowNames.desktop);
 
 export const desktopAppTitle = `${NWMM_APP_NAME} ${NWMM_APP_VERSION}`;
 document.title = desktopAppTitle;
