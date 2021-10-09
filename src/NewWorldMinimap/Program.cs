fde624c332c91302e43d2d06eb76d466985c11e0
Mikael Turøy <mikael.turoy@holbergeeg.org>
2021-10-09 16:30:46 +0200

    Made program DPI aware

--- src/NewWorldMinimap/Program.cs
+++ src/NewWorldMinimap/Program.cs
@@ -8,6 +8,10 @@ namespace NewWorldMinimap
     /// </summary>
     public static class Program
     {
+        //https://stackoverflow.com/questions/32438736/taking-a-screenshot-using-graphics-copyfromscreen-with-150-scaling/37800963
+        [System.Runtime.InteropServices.DllImport("user32.dll")]
+        public static extern bool SetProcessDPIAware();
+
         /// <summary>
         /// Defines the entry point of the application.
         /// </summary>
@@ -15,6 +19,7 @@ namespace NewWorldMinimap
         [STAThread]
         public static void Main(string[] args)
         {
+            SetProcessDPIAware();
             Application.EnableVisualStyles();
             using Form map = new MapForm();
             Application.Run(map);
