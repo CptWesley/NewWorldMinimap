c720865cf9ccfcd3ff30be78c1a9c143ceef56fa
Wesley Baartman <w.j.baartman@student.tudelft.nl>
2021-10-10 13:08:16 +0200

    Refactor map util functions

--- overwolf/src/Global.d.ts
+++ overwolf/src/Global.d.ts
@@ -2,7 +2,7 @@ declare const NWMM_APP_NAME: string;
 declare const NWMM_APP_VERSION: string;
 declare const NWMM_APP_BUILD_DATE: string;
 
-declare type Point = {
+declare type Vector2 = {
     x: number;
     y: number;
 };
