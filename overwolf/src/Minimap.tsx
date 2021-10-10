fa2208dc9af245ba64f76273411a543c860e29b9
Wesley Baartman <w.j.baartman@student.tudelft.nl>
2021-10-10 20:17:27 +0200

    Change drawing code to be a little more responsive

--- overwolf/src/Minimap.tsx
+++ overwolf/src/Minimap.tsx
@@ -23,41 +23,79 @@ export default function Minimap() {
 
     const [position, setPosition] = useState<Vector2>({ x: 7728.177, y: 1988.299 });
     const canvas = useRef<HTMLCanvasElement>(null);
+
+    let drawing = false;
+    let interrupt = false;
+
     const draw = async () => {
         const ctx = canvas.current?.getContext('2d');
-        if (ctx) {
-            ctx.canvas.width = ctx.canvas.clientWidth;
-            ctx.canvas.height = ctx.canvas.clientHeight;
 
-            const centerX = ctx.canvas.width / 2;
-            const centerY = ctx.canvas.height / 2;
+        if (!ctx) {
+            return;
+        }
 
-            const tiles = getTiles(position, ctx.canvas.width, ctx.canvas.height);
-            const offset = toMinimapCoordinate(position, position, ctx.canvas.width, ctx.canvas.height);
+        if (!drawing) {
+            drawing = false;
+            interrupt = false;
+        }
 
-            for (let x = 0; x < tiles.length; x++) {
-                const row = tiles[x];
-                for (let y = 0; y < row.length; y++) {
-                    const tile = row[y];
-                    const bitmap = await tile.image;
-                    ctx.drawImage(await tile.image,
-                        bitmap.width * x + Math.floor(centerX) - offset.x,
-                        bitmap.height * y + Math.floor(centerY) - offset.y
-                    );
+        if (drawing) {
+            interrupt = true;
+        }
 
-                    for (const marker of await tile.markers) {
-                        const imgPos = toMinimapCoordinate(position, marker.pos, ctx.canvas.width, ctx.canvas.height);
-                        const imgPosCorrected = { x: imgPos.x - offset.x + centerX, y: imgPos.y - offset.y + centerY };
-                        ctx.fillStyle = 'red';
-                        ctx.fillRect(imgPosCorrected.x, imgPosCorrected.y, 8, 8);
+        drawing = true;
+
+        ctx.canvas.width = ctx.canvas.clientWidth;
+        ctx.canvas.height = ctx.canvas.clientHeight;
+
+        const centerX = ctx.canvas.width / 2;
+        const centerY = ctx.canvas.height / 2;
+
+        const tiles = getTiles(position, ctx.canvas.width, ctx.canvas.height);
+        const offset = toMinimapCoordinate(position, position, ctx.canvas.width, ctx.canvas.height);
+
+        for (let x = 0; x < tiles.length; x++) {
+            const row = tiles[x];
+            for (let y = 0; y < row.length; y++) {
+                const tile = row[y];
+                const bitmap = await tile.image;
+
+                if (interrupt) {
+                    interrupt = false;
+                    return;
+                }
+
+                ctx.drawImage(await tile.image,
+                    bitmap.width * x + Math.floor(centerX) - offset.x,
+                    bitmap.height * y + Math.floor(centerY) - offset.y
+                );
+
+                for (const marker of await tile.markers) {
+                    const imgPos = toMinimapCoordinate(position, marker.pos, ctx.canvas.width, ctx.canvas.height);
+                    const imgPosCorrected = { x: imgPos.x - offset.x + centerX, y: imgPos.y - offset.y + centerY };
+                    ctx.fillStyle = 'red';
+
+                    if (interrupt) {
+                        interrupt = false;
+                        return;
                     }
+
+                    ctx.fillRect(imgPosCorrected.x, imgPosCorrected.y, 8, 8);
                 }
             }
-
-            if (playerDebugIcon) {
-                ctx.drawImage(playerDebugIcon, Math.floor(centerX - playerDebugIcon.width / 2), Math.floor(centerY - playerDebugIcon.height / 2));
-            }
         }
+
+        if (playerDebugIcon) {
+            if (interrupt) {
+                interrupt = false;
+                return;
+            }
+
+            ctx.drawImage(playerDebugIcon, Math.floor(centerX - playerDebugIcon.width / 2), Math.floor(centerY - playerDebugIcon.height / 2));
+        }
+
+        drawing = false;
+        interrupt = false;
     };
 
     // Store the `draw` function in a ref object, so we can always access the latest one.
