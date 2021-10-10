013536723ab68e2bf22a4134d4399baec974207a
Wesley Baartman <w.j.baartman@student.tudelft.nl>
2021-10-10 13:08:16 +0200

    Refactor map util functions

--- overwolf/src/App.tsx
+++ overwolf/src/App.tsx
@@ -1,5 +1,5 @@
 import React, { useEffect, useRef } from 'react';
-import { getTileBitmap, getTiles } from './logic/tiles';
+import { getTileBitmap, getTiles, toMinimapCoordinate } from './logic/tiles';
 import { makeStyles } from './theme';
 
 const useStyles = makeStyles()({
@@ -21,16 +21,28 @@ export default function App() {
                 ctx.canvas.width = ctx.canvas.clientWidth;
                 ctx.canvas.height = ctx.canvas.clientHeight;
 
+                const playerPos = { x: 7728.177, y: 1988.299 };
+                const radius = 1;
+
                 // const bitmap = await getTileBitmap(109, 205);
                 // ctx.drawImage(bitmap, 10, 10);
-                const bitmaps = getTiles(7728.177, 1988.299);
+                const bitmaps = getTiles(playerPos, radius);
+                const offset = toMinimapCoordinate(playerPos, playerPos, radius);
+                console.log(offset);
+                console.log('width: ' + ctx.canvas.width + ' height: ' + ctx.canvas.height);
 
                 for (let x = 0; x < bitmaps.length; x++) {
-                    for (let y = 0; y < bitmaps.length; y++) {
-                        const bitmap = await bitmaps[x][y];
-                        ctx.drawImage(bitmap, bitmap.width * x, bitmap.height * y);
+                    const row = bitmaps[x];
+                    for (let y = 0; y < row.length; y++) {
+                        const bitmap = await row[y];
+                        ctx.drawImage(bitmap,
+                            bitmap.width * x + Math.floor(ctx.canvas.width / 2) - offset.x,
+                            bitmap.height * y + Math.floor(ctx.canvas.height / 2) - offset.y);
                     }
                 }
+
+                ctx.fillStyle = 'red';
+                ctx.fillRect(ctx.canvas.width / 2 - 16, ctx.canvas.height / 2 - 16, 32, 32);
             }
         }
 
--- overwolf/src/logic/tiles.ts
+++ overwolf/src/logic/tiles.ts
@@ -4,14 +4,13 @@ const tileWidth = 256;
 const tileHeight = 256;
 const gameMapWidth = 14336;
 const gameMapHeight = 14400;
-const radius = 2;
 
-export function getTileCoordinatesForWorldCoordinate(x: number, y: number) {
+export function getTileCoordinatesForWorldCoordinate(worldPos: Vector2) {
     const totalWidth = width * tileWidth;
     const totalHeight = height * tileHeight;
 
-    const imageX = x / gameMapWidth * totalWidth;
-    const imageY = (gameMapHeight - y) / gameMapHeight * totalHeight;
+    const imageX = worldPos.x / gameMapWidth * totalWidth;
+    const imageY = (gameMapHeight - worldPos.y) / gameMapHeight * totalHeight;
 
     const tileX = Math.floor(imageX / tileWidth);
     const tileY = Math.floor(imageY / tileHeight);
@@ -19,12 +18,12 @@ export function getTileCoordinatesForWorldCoordinate(x: number, y: number) {
     return { x: tileX, y: tileY - 1 };
 }
 
-function getTileImageUrl(x: number, y: number) {
-    return `https://cdn.newworldfans.com/newworldmap/8/${x}/${y}.png`;
+function getTileImageUrl(tilePos: Vector2) {
+    return `https://cdn.newworldfans.com/newworldmap/8/${tilePos.x}/${tilePos.y}.png`;
 }
 
-async function getTileBitmapFromServer(x: number, y: number) {
-    const imageUrl = getTileImageUrl(x, y);
+async function getTileBitmapFromServer(tilePos: Vector2) {
+    const imageUrl = getTileImageUrl(tilePos);
     console.log(imageUrl);
     const imageRequest = await fetch(imageUrl, {
         method: 'get',
@@ -34,17 +33,17 @@ async function getTileBitmapFromServer(x: number, y: number) {
     return bitmap;
 }
 
-function getTileCacheKey(x: number, y: number) {
-    return `${x},${y}`;
+function getTileCacheKey(tilePos: Vector2) {
+    return `${tilePos.x},${tilePos.y}`;
 }
 
-export function toMinimapCoordinate(playerX: number, playerY: number, x: number, y: number) {
+export function toMinimapCoordinate(playerWorldPos: Vector2, worldPos: Vector2, radius: number) {
     const totalWidth = tileWidth * width;
     const totalHeight = tileHeight * height;
-    const { x: tileX, y: tileY } = getTileCoordinatesForWorldCoordinate(playerX, playerY);
+    const { x: tileX, y: tileY } = getTileCoordinatesForWorldCoordinate(playerWorldPos);
 
-    const pixelX = Math.floor(x / gameMapWidth * totalWidth);
-    const pixelY = Math.floor((gameMapHeight - y) / gameMapHeight * totalHeight);
+    const pixelX = Math.floor(worldPos.x / gameMapWidth * totalWidth);
+    const pixelY = Math.floor((gameMapHeight - worldPos.y) / gameMapHeight * totalHeight);
 
     const imageX = pixelX - ((tileX - radius) * tileWidth);
     const imageY = pixelY - ((tileY - radius + 1) * tileHeight);
@@ -53,32 +52,29 @@ export function toMinimapCoordinate(playerX: number, playerY: number, x: number,
 }
 
 const tileBitmapCache = new Map<string, Promise<ImageBitmap>>();
-export async function getTileBitmap(x: number, y: number) {
-    const key = getTileCacheKey(x, y);
+export async function getTileBitmap(pos: Vector2) {
+    const key = getTileCacheKey(pos);
 
     let bitmapPromise = tileBitmapCache.get(key);
     if (!bitmapPromise) {
-        bitmapPromise = getTileBitmapFromServer(x, y);
+        bitmapPromise = getTileBitmapFromServer(pos);
         tileBitmapCache.set(key, bitmapPromise);
     }
 
     return await bitmapPromise;
 }
 
-export function getTiles(x: number, y: number) {
+export function getTiles(worldPos: Vector2, radius: number) {
     const dimension = radius * 2 + 1;
     const result: Promise<ImageBitmap>[][] = [];
 
-    const { x: centerX, y: centerY } = getTileCoordinatesForWorldCoordinate(x, y);
-
-    console.log('x: ' + centerX);
-    console.log('y: ' + centerY);
+    const tilePos = getTileCoordinatesForWorldCoordinate(worldPos);
 
     for (let x = 0; x < dimension; ++x) {
         const col: Promise<ImageBitmap>[] = [];
         result.push(col);
         for (let y = 0; y < dimension; ++y) {
-            col.push(getTileBitmap(centerX - radius + x, centerY - radius + y));
+            col.push(getTileBitmap({ x: tilePos.x - radius + x, y: tilePos.y - radius + y }));
         }
     }
 
