172adc64b43950527d461dae8e06fee566ac6dc1
pvdstel <pvdstel@users.noreply.github.com>
2021-10-12 20:20:46 +0200

    Add app to in-game window

---
+++ overwolf/src/InGame.tsx
@@ -0,0 +1,11 @@
+import React from 'react';
+import App from './App';
+import Frame from './Frame';
+import InGameHeader from './InGameHeader';
+
+export default function Desktop() {
+    return <Frame
+        header={<InGameHeader />}
+        content={<App />}
+    />;
+}
