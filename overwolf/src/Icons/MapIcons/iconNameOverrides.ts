1fc4c393f280a92f6ca256820234a14e710b5117
Wesley Baartman <w.j.baartman@student.tudelft.nl>
2021-10-14 18:25:46 +0200

    Improve chest name selection

--- overwolf/src/Icons/MapIcons/iconNameOverrides.ts
+++ overwolf/src/Icons/MapIcons/iconNameOverrides.ts
@@ -17,28 +17,4 @@ export const iconNameOverrides = {
     'wolf_elemental': 'Elemental Wolf',
 
     'monarchs_bluffs': 'Monarch\'s Bluffs',
-
-    'se1': 'Supply Stockpile T1+',
-    'se2': 'Supply Stockpile T2+',
-    'se3': 'Supply Stockpile T3+',
-    'se4': 'Supply Stockpile T4+',
-    'se5': 'Supply Stockpile T5+',
-
-    'ss1': 'Supply Cache T1',
-    'ss2': 'Supply Cache T2',
-    'ss3': 'Supply Cache T3',
-    'ss4': 'Supply Cache T4',
-    'ss5': 'Supply Cache T5',
-
-    'sl1': 'Supply Stockpile T1',
-    'sl2': 'Supply Stockpile T2',
-    'sl3': 'Supply Stockpile T3',
-    'sl4': 'Supply Stockpile T4',
-    'sl5': 'Supply Stockpile T5',
-
-    'sm1': 'Supply Crate T1',
-    'sm2': 'Supply Crate T2',
-    'sm3': 'Supply Crate T3',
-    'sm4': 'Supply Crate T4',
-    'sm5': 'Supply Crate T5',
 };
--- overwolf/src/logic/icons.ts
+++ overwolf/src/logic/icons.ts
@@ -36,7 +36,39 @@ export async function GetPlayerIcon() {
     return iconCache.player;
 }
 
+function getChestCategoryName(str: string) {
+    switch (str.charAt(0)) {
+        case 's': return 'Supply';
+        case 'c': return 'Provisions';
+        case 'o': return 'Ancient';
+        case 'a': return 'Alchemy';
+        default: return undefined;
+    }
+}
+
+function getChestTypeName(str: string) {
+    switch (str.charAt(1)) {
+        case 'e': return 'Elite Stockpile';
+        case 'l': return 'Stockpile';
+        case 's': return 'Cache';
+        case 'm': return 'Crate';
+        default: return undefined;
+    }
+}
+
 function predictCorrectName(str: string) {
+    const matches = str.match(/^\w\w\d$/);
+
+    if (matches && matches.length === 1) {
+        const match = matches[0];
+        const cat = getChestCategoryName(match);
+        const type = getChestTypeName(match);
+
+        if (cat && type) {
+            return cat + ' ' + type + ' T' + match.charAt(2);
+        }
+    }
+
     const parts = str.split('_');
     const capitalizedParts = parts.map(x => x.charAt(0).toUpperCase() + x.slice(1));
     return capitalizedParts.join(' ');
