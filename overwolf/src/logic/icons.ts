5f342d45d45ad65e0296cc87eada34ee1f07802d
Wesley Baartman <w.j.baartman@student.tudelft.nl>
2021-10-14 18:00:28 +0200

    Move overrides to seperate file

--- overwolf/src/logic/icons.ts
+++ overwolf/src/logic/icons.ts
@@ -1,3 +1,4 @@
+import { iconNameOverrides } from '../Icons/MapIcons/iconNameOverrides';
 import { createMapIconCache } from '../Icons/MapIcons/mapIconCache';
 
 let iconCachePromise = createMapIconCache(2.5);
@@ -35,27 +36,6 @@ export async function GetPlayerIcon() {
     return iconCache.player;
 }
 
-const iconNames = {
-    'npc': 'NPCs',
-    'chests': 'Chests',
-    'documents': 'Documents',
-    'essences': 'Essences',
-    'fishing': 'Fish',
-    'monsters': 'Creatures',
-    'ores': 'Ores',
-    'plants': 'Plants',
-    'pois': 'Places of Interest',
-    'woods': 'Trees',
-
-    'bear_elemental': 'Elemental Bear',
-    'elk_elemental': 'Elemental Elk',
-    'named': 'Boss',
-    'turkey_nest': 'Turkey Nest',
-    'wolf_elemental': 'Elemental Wolf',
-
-    'monarchs_bluffs': 'Monarch\'s Bluffs',
-};
-
 function predictCorrectName(str: string) {
     const parts = str.split('_');
     const capitalizedParts = parts.map(x => x.charAt(0).toUpperCase() + x.slice(1));
@@ -63,5 +43,5 @@ function predictCorrectName(str: string) {
 }
 
 export function getIconName(type: string) {
-    return iconNames[type] ?? predictCorrectName(type);
+    return iconNameOverrides[type] ?? predictCorrectName(type);
 }
