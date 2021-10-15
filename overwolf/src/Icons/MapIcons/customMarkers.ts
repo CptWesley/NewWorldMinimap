9ab54746bf11f7836f17fac2b57bc22e267a90da
Wesley Baartman <w.j.baartman@student.tudelft.nl>
2021-10-15 16:24:38 +0200

    Add first light town markers

--- overwolf/src/Icons/MapIcons/customMarkers.ts
+++ overwolf/src/Icons/MapIcons/customMarkers.ts
@@ -5,11 +5,19 @@ export const customMarkers = {
                 x: 7910,
                 y: 1880,
             },
+            'first_light': {
+                x: 8858,
+                y: 635,
+            },
         },
         'covenant_adjudicator': {
             'cutlass_keys': {
                 x: 7970,
                 y: 1910,
+            },
+            'first_light': {
+                x: 8902,
+                y: 683,
             },
         },
         'marauder_commander': {
@@ -17,11 +25,19 @@ export const customMarkers = {
                 x: 7917,
                 y: 1959,
             },
+            'first_light': {
+                x: 8804,
+                y: 738,
+            },
         },
         'arcane_repository': {
             'cutlass_keys': {
                 x: 7954,
                 y: 1885,
+            },
+            'first_light': {
+                x: 8863,
+                y: 675,
             },
         },
         'forge': {
@@ -29,11 +45,19 @@ export const customMarkers = {
                 x: 7909,
                 y: 1906,
             },
+            'first_light': {
+                x: 8837,
+                y: 655,
+            },
         },
         'inn': {
             'cutlass_keys': {
                 x: 7893,
                 y: 1940,
+            },
+            'first_light': {
+                x: 8848,
+                y: 697,
             },
         },
         'loom': {
@@ -41,11 +65,19 @@ export const customMarkers = {
                 x: 7939,
                 y: 1881,
             },
+            'first_light': {
+                x: 8909,
+                y: 714,
+            },
         },
         'outfitting_station': {
             'cutlass_keys': {
                 x: 7937,
                 y: 1892,
+            },
+            'first_light': {
+                x: 8905,
+                y: 706,
             },
         },
         'stonecutting_table': {
@@ -53,11 +85,19 @@ export const customMarkers = {
                 x: 7900,
                 y: 1910,
             },
+            'first_light': {
+                x: 8870,
+                y: 623,
+            },
         },
-        'territory_board': {
+        'territory_planning_board': {
             'cutlass_keys': {
                 x: 7929,
                 y: 1887,
+            },
+            'first_light': {
+                x: 8854,
+                y: 765,
             },
         },
         'trading_post': {
@@ -65,11 +105,19 @@ export const customMarkers = {
                 x: 7946,
                 y: 1918,
             },
+            'first_light': {
+                x: 8842,
+                y: 714,
+            },
         },
         'woodshop': {
             'cutlass_keys': {
                 x: 7918,
                 y: 1931,
+            },
+            'first_light': {
+                x: 8853,
+                y: 639,
             },
         },
         'governors_desk': {
@@ -77,17 +125,29 @@ export const customMarkers = {
                 x: 7919,
                 y: 1880,
             },
+            'first_light': {
+                x: 8866,
+                y: 761,
+            },
         },
         'kitchen': {
             'cutlass_keys': {
                 x: 7947,
                 y: 1891,
             },
+            'first_light': {
+                x: 8915,
+                y: 717,
+            },
         },
         'smelter': {
             'cutlass_keys': {
                 x: 7924,
                 y: 1943,
+            },
+            'first_light': {
+                x: 8846,
+                y: 645,
             },
         },
         'storage_shed': {
@@ -99,14 +159,26 @@ export const customMarkers = {
                 x: 7993,
                 y: 1861,
             },
+            'first_light_1': {
+                x: 8898,
+                y: 692,
+            },
+            'first_light_2': {
+                x: 8861,
+                y: 628,
+            },
         },
         'tannery': {
             'cutlass_keys': {
                 x: 7912,
                 y: 1948,
             },
+            'first_light': {
+                x: 8854,
+                y: 669,
+            },
         },
-        'town_board': {
+        'town_project_board': {
             'cutlass_keys_1': {
                 x: 7914,
                 y: 1892,
@@ -114,6 +186,14 @@ export const customMarkers = {
             'cutlass_keys_2': {
                 x: 7932,
                 y: 1878,
+            },
+            'first_light_1': {
+                x: 8847,
+                y: 750,
+            },
+            'first_light_2': {
+                x: 8858,
+                y: 772,
             },
         },
         'war_board': {
@@ -129,11 +209,27 @@ export const customMarkers = {
                 x: 6960,
                 y: 1224,
             },
+            'first_light_1': {
+                x: 8840,
+                y: 758,
+            },
+            'first_light_2': {
+                x: 9966,
+                y: 1258,
+            },
+            'first_light_3': {
+                x: 9873,
+                y: 1196,
+            },
         },
         'workshop': {
             'cutlass_keys': {
                 x: 7928,
                 y: 1932,
+            },
+            'first_light': {
+                x: 8879,
+                y: 626,
             },
         },
     },
