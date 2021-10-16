202e469c9e6f4d4251b03d986b24fae3258a5ced
AlessioDP <alessiodptv@gmail.com>
2021-10-16 02:24:26 +0200

    Added select/unselect all icons by category

---
+++ overwolf/src/Icons/SelectIcon.tsx
@@ -0,0 +1,10 @@
+import React from 'react';
+
+export default function UnselectIcon() {
+    return (
+        <svg viewBox="0 0 40 40">
+            <path d="M 10,20 20,30 30,10" fill="none" stroke="currentcolor" strokeWidth="3"></path>
+            <rect x="5" y="5" width="30" height="30" rx="5" ry="5" fill="none" stroke="currentcolor" strokeWidth="3"></rect>
+        </svg>
+    );
+}
