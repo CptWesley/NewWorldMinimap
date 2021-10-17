6644afdd8dfb8da0cb6f0a23d7ea6fa2537d036e
AlessioDP <alessiodptv@gmail.com>
2021-10-17 19:55:56 +0200

    Added marker names

--- overwolf/src/Icons/MapIcons/iconNameOverrides.ts
+++ overwolf/src/Icons/MapIcons/iconNameOverrides.ts
@@ -1,21 +1,39 @@
-export const iconNameOverrides = {
-    'npc': 'NPCs',
-    'chests': 'Chests',
-    'documents': 'Documents',
-    'essences': 'Essences',
-    'fishing': 'Fish',
-    'monsters': 'Creatures',
-    'ores': 'Ores',
-    'plants': 'Plants',
-    'pois': 'Places of Interest',
-    'woods': 'Trees',
+import { customCategoryNamesEn, customNamesEn, iconNamesEn } from './names/en';
 
-    'bear_elemental': 'Elemental Bear',
-    'elk_elemental': 'Elemental Elk',
-    'named': 'Boss',
-    'turkey_nest': 'Turkey Nest',
-    'wolf_elemental': 'Elemental Wolf',
+const temporaryNames = {};
 
-    'monarchs_bluffs': 'Monarch\'s Bluffs',
-    'weavers_fen': 'Weaver\'s Fen',
-};
+export function getIconNameOverride(category: string, type?: string) {
+    if (type) {
+        if (temporaryNames[getTemporaryKey(category, type)]) {
+            return temporaryNames[getTemporaryKey(category, type)];
+        }
+
+        // Categories override
+        if (category === 'pois' && iconNamesEn['regions'][type]) {
+            // if the poi is a region, return region name instead
+            return iconNamesEn['regions'][type];
+        }
+        if (category === 'npcs' && type && iconNamesEn['regions'][type]) {
+            // npcs may contain region names
+            return iconNamesEn['regions'][type];
+        }
+
+        if (customNamesEn[category] && customNamesEn[category][type]) {
+            return customNamesEn[category][type];
+        }
+        if (iconNamesEn[category]) {
+            return iconNamesEn[category][type];
+        }
+    } else {
+        return customCategoryNamesEn[category];
+    }
+    return undefined;
+}
+
+export function saveTemporaryIconName(category: string, type: string | undefined, prediction: string) {
+    temporaryNames[getTemporaryKey(category, type)] = prediction;
+}
+
+function getTemporaryKey(category: string, type: string | undefined) {
+    return category + ':' + type;
+}
