e7cb34c68080a784618767de68117b7640d6bbd9
pvdstel <pvdstel@users.noreply.github.com>
2021-10-19 12:43:08 +0200

    Remove unnecessary styling

--- overwolf/src/DiscordButton.tsx
+++ overwolf/src/DiscordButton.tsx
@@ -11,33 +11,16 @@ interface IProps {
 const useStyles = makeStyles()(theme => ({
     root: {
         display: 'flex',
-        position: 'relative',
     },
     discordLink: {
         color: theme.color,
         textDecoration: 'none',
     },
-    icon: {
-        width: '1em',
-        height: '1em',
-        position: 'absolute',
-        top: '0.1em',
-        left: 0,
-    },
     text: {
-        position: 'relative',
-        zIndex: 2,
-        width: '100%',
-        appearance: 'none',
-        border: 'none',
-        background: 'none',
-        color: 'currentcolor',
-
-        fontFamily: theme.bodyFontFamily,
         fontSize: typeof theme.bodyFontSize === 'number'
             ? theme.bodyFontSize - 2
             : `calc(${theme.bodyFontSize} - 2px)`,
-        padding: '2px 4px 2px 5px',
+        marginLeft: 5,
     },
 }));
 
