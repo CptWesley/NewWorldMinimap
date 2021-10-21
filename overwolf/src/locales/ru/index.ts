28b8c4ae6b47d720be9e76966e1a2e902139278d
wellzar <alexander.taylor@yandex.ru>
2021-10-21 18:32:01 +0300

    'export const ru' instead of 'en'

--- overwolf/src/locales/ru/index.ts
+++ overwolf/src/locales/ru/index.ts
@@ -2,7 +2,7 @@ import common from './common.json';
 import markers from './markers.json';
 import customMarkers from './customMarkers.json';
 
-export const en = {
+export const ru = {
     common,
     markers: {
         ...markers,
