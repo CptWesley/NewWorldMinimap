675a4d0047abcc0d8aba06e0d9b82994ff21e5d9
wellzar <alexander.taylor@yandex.ru>
2021-10-21 18:42:35 +0300

    deleted missing imports

--- overwolf/src/locales/ru/index.ts
+++ overwolf/src/locales/ru/index.ts
@@ -1,11 +1,5 @@
 import common from './common.json';
-import markers from './markers.json';
-import customMarkers from './customMarkers.json';
 
 export const ru = {
-    common,
-    markers: {
-        ...markers,
-        custom: customMarkers,
-    },
+    common
 };
