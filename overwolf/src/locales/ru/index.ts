90c3e325036550ffee3901244bc97a9cb79f72fb
Wesley Baartman <w.j.baartman@student.tudelft.nl>
2021-10-22 08:40:16 +0200

    Fix Russian and Romanian localisation not using the customMarkers.json

--- overwolf/src/locales/ru/index.ts
+++ overwolf/src/locales/ru/index.ts
@@ -1,5 +1,9 @@
 import common from './common.json';
+import customMarkers from './customMarkers.json';
 
 export const ru = {
     common,
+    markers: {
+        custom: customMarkers,
+    },
 };
