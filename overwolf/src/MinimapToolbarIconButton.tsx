f8e904a24fb6c2557e5d40777b7c094af6af8c35
pvdstel <pvdstel@users.noreply.github.com>
2021-10-30 20:47:12 +0200

    Disable interaction with IconButton SVGs

--- overwolf/src/MinimapToolbarIconButton.tsx
+++ overwolf/src/MinimapToolbarIconButton.tsx
@@ -26,6 +26,7 @@ const useStyles = makeStyles()(theme => ({
         '& > svg': {
             width: '100%',
             height: '100%',
+            pointerEvents: 'none', // makes tooltips behave nicer
         },
 
         '&:hover, &:focus': {
