40f7398930152fb0724ad8dbc7750f513196861d
pvdstel <pvdstel@users.noreply.github.com>
2021-10-31 11:44:47 +0100

    Use Lato for map text if available
    Still uses sans-serif as a fallback

--- overwolf/src/Minimap/setTextStyle.ts
+++ overwolf/src/Minimap/setTextStyle.ts
@@ -1,6 +1,6 @@
 export default function setTextStyle(ctx: CanvasRenderingContext2D, iconScale: number) {
     ctx.textAlign = 'center';
-    ctx.font = Math.round(iconScale * 10) + 'px sans-serif';
+    ctx.font = Math.round(iconScale * 10) + 'px Lato, sans-serif';
     ctx.strokeStyle = '#000';
     ctx.fillStyle = '#fff';
     ctx.lineWidth = 2.5;
