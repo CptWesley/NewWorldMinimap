4b8dae51c418d9b6cf126d6f845dafadd0161586
pvdstel <pvdstel@users.noreply.github.com>
2021-10-31 11:52:04 +0100

    Fix player drawing to use map position

--- overwolf/src/Minimap/drawMapPlayer.ts
+++ overwolf/src/Minimap/drawMapPlayer.ts
@@ -1,4 +1,5 @@
 import { toMinimapCoordinate } from '@/logic/tiles';
+import { rotateAround } from '@/logic/util';
 import { MapIconRendererParameters, MapRendererParameters } from './useMinimapRenderer';
 
 export default function drawMapPlayer(params: MapRendererParameters, iconParams: MapIconRendererParameters) {
@@ -19,23 +20,32 @@ export default function drawMapPlayer(params: MapRendererParameters, iconParams:
     } = iconParams;
 
     const playerIcon = mapIconsCache.getPlayerIcon();
+    if (!playerIcon) {
+        return;
+    }
 
-    if (playerIcon) {
-        if (renderAsCompass) {
-            ctx.drawImage(playerIcon, center.x - playerIcon.width / 2, center.y - playerIcon.height / 2);
-        } else {
-            const mapPos = toMinimapCoordinate(mapCenterPosition, playerPosition, ctx.canvas.width, ctx.canvas.height, zoomLevel, 1);
-            const imgPosCorrected = {
-                x: mapPos.x / zoomLevel - offset.x / zoomLevel + center.x,
-                y: mapPos.y / zoomLevel - offset.y / zoomLevel + center.y,
-            };
-            ctx.save();
-            ctx.translate(imgPosCorrected.x, imgPosCorrected.y);
-            ctx.rotate(angle);
-            ctx.translate(-imgPosCorrected.x, -imgPosCorrected.y);
-            ctx.drawImage(playerIcon, imgPosCorrected.x - playerIcon.width / 2,
-                imgPosCorrected.y - playerIcon.height / 2);
-            ctx.restore();
-        }
+    const mapPos = toMinimapCoordinate(
+        mapCenterPosition,
+        playerPosition,
+        ctx.canvas.width,
+        ctx.canvas.height,
+        zoomLevel,
+        1);
+    const imgPosCorrected = {
+        x: mapPos.x / zoomLevel - offset.x / zoomLevel + center.x,
+        y: mapPos.y / zoomLevel - offset.y / zoomLevel + center.y,
+    };
+
+    if (renderAsCompass) {
+        const rotated = rotateAround(center, imgPosCorrected, -angle);
+        ctx.drawImage(playerIcon, rotated.x - playerIcon.width / 2, rotated.y - playerIcon.height / 2);
+    } else {
+        ctx.save();
+        ctx.translate(imgPosCorrected.x, imgPosCorrected.y);
+        ctx.rotate(angle);
+        ctx.translate(-imgPosCorrected.x, -imgPosCorrected.y);
+        ctx.drawImage(playerIcon, imgPosCorrected.x - playerIcon.width / 2,
+            imgPosCorrected.y - playerIcon.height / 2);
+        ctx.restore();
     }
 }
